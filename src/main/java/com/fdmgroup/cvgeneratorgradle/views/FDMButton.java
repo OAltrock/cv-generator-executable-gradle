aca218990d1eae87ccc9f398159cb638612f0f27
nickpapenheim <nickpapenheim@gmail.com>
2024-04-26 00:12:08 +0200

    added stylizable buttons

--- src/main/java/com/fdmgroup/cvgeneratorgradle/views/FDMButton.java
+++ src/main/java/com/fdmgroup/cvgeneratorgradle/views/FDMButton.java
@@ -8,6 +8,10 @@ public class FDMButton extends Button {
     public void setDesign() {
         setMinWidth(USE_PREF_SIZE);
     }
+    
+    public void setDesign(String style) {
+        configure(style);
+    }
 
     public FDMButton() {
     }
@@ -18,5 +22,43 @@ public class FDMButton extends Button {
 
     public FDMButton(String s, Node node) {
         super(s, node);
+    }
+    
+    
+    
+    // configures style according to style 
+    private void configure(String style) {
+    	
+        // Set specific style based on the input parameter
+        if (style.equals("primary")) {
+        	this.setStyle("-fx-padding: 10px;" +
+		                    "-fx-text-align: center;" +
+		                    "-fx-font-size: 16px;" +
+		                    "-fx-cursor: hand;" +
+		                    "-fx-background-radius: 2vh;" + 
+		                    "-fx-background-color: #C5FF00;" +
+                            "-fx-text-fill: black;" +
+                            "-fx-font-weight: bold;" +
+                            "-fx-border-color: transparent;");
+        } else if (style.equals("secondary")) {
+        	this.setStyle("-fx-padding: 10px;" +
+		                    "-fx-text-align: center;" +
+		                    "-fx-font-size: 16px;" +
+		                    "-fx-cursor: hand;" +
+		                    "-fx-background-radius: 2vh;" + 
+		                    "-fx-background-color: #E9EDF6;" +
+                            "-fx-text-fill: black;" +
+                            "-fx-border-color: transparent;");
+        } else if (style.equals("tertiary")) {
+        	this.setStyle("-fx-padding: 10px;" +
+		                    "-fx-text-align: center;" +
+		                    "-fx-font-size: 16px;" +
+		                    "-fx-cursor: hand;" +
+		                    "-fx-background-radius: 2vh;" + 
+		                    "-fx-background-color: transparent;" +
+                            "-fx-text-fill: white;" +
+                            "-fx-border-color: #C5FF00;" +
+                            "-fx-border-width: 1px;");
+        }
     }
 }
